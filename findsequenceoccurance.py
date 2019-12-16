24b7718b1d5e28499b123e99e0221895629a7800
tejesh <tejesh.789@gmail.com>
2019-12-16 20:32:52 +0530

    Create findsequenceoccurance.py
    Finds number of sequence present in a given input

---
+++ findsequenceoccurance.py
@@ -0,0 +1,20 @@
+arr = "abcxyabcdefghdsafeabcdabc"
+start = 0
+pointer = start + 1
+length = len(arr)
+test = {}
+temp = 98
+while start < pointer and pointer < length:
+    while pointer < length and chr(temp) == arr[pointer]:
+        temp = temp + 1
+        pointer = pointer + 1
+    key = arr[start: pointer]
+    if len(key) > 1:
+    	if test.has_key(key):
+        	test[key] = test[key] + 1
+    	else:
+        	test[key] = 1
+    start = pointer
+    pointer = pointer + 1
+    temp = 98
+print test
