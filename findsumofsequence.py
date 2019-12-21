8a091acfac5f8dee738887ac57da9ea7af6eda30
tejesh <tejesh.789@gmail.com>
2019-12-21 11:11:44 +0530

    Create findsumofsequence.py

---
+++ findsumofsequence.py
@@ -0,0 +1,15 @@
+#Given an unsorted array A of size N of non-negative integers, find a continuous sub-array which adds to a given number S.
+
+def findsubarray(length,sum,input):
+    j = 0
+    current_sum = 0
+    while j < length:
+        i = j
+        while current_sum < sum and i < length:
+            current_sum = current_sum + input[i]
+            if current_sum == sum:
+                print j,i
+            i = i +1
+        current_sum = 0
+        j = j + 1
+findsubarray(10,15,[1,2,3,4,5,6,7,8,9,10])
